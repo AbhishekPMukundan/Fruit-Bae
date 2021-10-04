af07037ce0f9d24a49f3e4f8d22eb9c079e97399
Abhishek <mailabhi390@gmail.com>
2021-10-04 15:49:46 +0530

    Initial Commit

---
+++ Fruit Bae/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruit Bae
+//
+//  Created by Abhishek on 04/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruit Bae/Fruit_BaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_BaeApp.swift
+//  Fruit Bae
+//
+//  Created by Abhishek on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_BaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
